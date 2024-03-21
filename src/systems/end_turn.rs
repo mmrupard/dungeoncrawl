30094c0a43a22c955c4374895d649b96c477f7e1
Morgan Rupard <17115504+mmrupard@users.noreply.github.com>
2024-03-20 17:46:12 -0700

    feat: added a victory screen when the player picks up the Amulet of Yala

--- src/systems/end_turn.rs
+++ src/systems/end_turn.rs
@@ -2,9 +2,13 @@ use crate::prelude::*;
 
 #[system]
 #[read_component(Health)]
+#[read_component(Point)]
 #[read_component(Player)]
+#[read_component(AmuletOfYala)]
 pub fn end_turn(ecs: &SubWorld, #[resource] turn_state: &mut TurnState) {
-    let mut player_hp = <&Health>::query().filter(component::<Player>());
+    let mut player_hp = <(&Health, &Point)>::query().filter(component::<Player>());
+    let mut amulet = <&Point>::query().filter(component::<AmuletOfYala>());
+    let amulet_pos = amulet.iter(ecs).next().unwrap();
     let current_state = *turn_state;
 
     let mut new_state = match current_state {
@@ -14,9 +18,13 @@ pub fn end_turn(ecs: &SubWorld, #[resource] turn_state: &mut TurnState) {
         _ => current_state,
     };
 
-    player_hp.iter(ecs).for_each(|hp| {
+    player_hp.iter(ecs).for_each(|(hp, pos)| {
         if hp.current < 1 {
             new_state = TurnState::GameOver;
+        }
+
+        if pos == amulet_pos {
+            new_state = TurnState::Victory;
         }
     });
 
